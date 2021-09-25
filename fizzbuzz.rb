6fa0090926cfaa013023935d52036d70dd7415e5
Malherbe <malherbe.billy@gmail.com>
2021-09-25 22:57:39 +0800

    YOn ti chofe

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+
+
+def fizzbuzz(size)
+    size.times{ |i| 
+    if (i%3==0 && i%5 ==0)
+        puts "FizzBuzz"
+    elsif (i%5 ==0)
+        puts "Buzz"
+    elsif(i%3==0)
+        puts "Fizz"
+    else
+        puts "#{i}"
+    end
+}
+end
+
+fizzbuzz(20)
